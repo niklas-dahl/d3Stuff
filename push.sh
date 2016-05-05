f7999c2b9ce8e49a92e8053b983f0b28d8e66341
Niklas <niklas-dahl@users.noreply.github.com>
2016-05-06 00:18:29 +0200

    added debug logs

--- push.sh
+++ push.sh
@@ -6,14 +6,17 @@ setup_git() {
 }
 
 commit_website_files() {
+  echo "Checkout branch"
   git checkout -b gh-pages
-  git pull
   git add ./dist
+  echo "Commit"
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
+  echo "add remote"
   git remote add origin-pages https://${GH_TOKEN}@github.com/niklas-dahl/d3Stuff.git > /dev/null 2>&1
+  echo "push"
   git push --quiet --set-upstream origin-pages gh-pages 
 }
 
