091ff1857f41e6ff68a637e88f0c5d522347d714
Niklas <niklas-dahl@users.noreply.github.com>
2016-05-06 16:42:53 +0200

    git checkout gh-pages without -b flag

--- push.sh
+++ push.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 commit_website_files() {
   echo "Checkout branch"
-  git checkout -b gh-pages
+  git checkout gh-pages
   git add ./dist
   echo "Commit"
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
